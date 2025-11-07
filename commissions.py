014850bc65cdefe16faf211ad499411f249732f8
hyshenn <135244915+hyshenn@users.noreply.github.com>
2025-11-07 11:25:26 +0200

    Update commissions.py

--- commissions.py
+++ commissions.py
@@ -1,8 +1,9 @@
 from system.lib import minescript
 from system.lib.java import JavaClass
 from detector import Detection
-from rotation import look_human, look_human_at #I preferred this one since i like it better, your choice tho
+from rotation import look #I preferred this one since i like it better, your choice tho
 from ThetaStar import path_walk_to
+from raycast import raycast_block_subregions
 import threading, time, math
 
 Minecraft = JavaClass("net.minecraft.client.Minecraft")
@@ -15,6 +16,10 @@ BLOCK_LIST = [
     "minecraft:prismarine",
     "minecraft:polished_diorite"
         ]
+
+def distance_3d(a, b):
+    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
+
 #Get the commissions from the tablist
 def get_commissions():
     connection = Minecraft.getInstance().getConnection()
@@ -47,11 +52,12 @@ def travel_to_commission():
     #Warp waypoints(It's most likely too fragile and exact might need to change it in the future)
     EATHER_WARPS = {
         "Lava Springs": [(31, 196, -10)],
-        "Upper Mines": [(-33, 175, -31), (-66, 223, -50), (-114, 197, -30)]
+        "Upper Mines": [(-33, 174, -31), (-66, 222, -50), (-114, 196, -30)]
     }
 
-    commissions = get_commissions()
-    area = next((a for c in commissions for a in AREA_LIST.values() if a in c.strip()), None)
+    #commissions = get_commissions()
+    #area = next((a for c in commissions for a in AREA_LIST.values() if a in c.strip()), None)
+    area = "Upper Mines"
     if not area:
         return
     #helpers
@@ -59,29 +65,39 @@ def travel_to_commission():
         for wx, wy, wz in EATHER_WARPS.get(area_name, []):
             minescript.player_press_sneak(True)
             time.sleep(0.2)
-            look_human_at(wx, wy, wz)
+            px, py, pz = minescript.player_position()
+            rays = raycast_block_subregions((px, py+1.52, pz), (wx, wy, wz), max_dist=500, rays_per_axis=4)
+            if rays:
+                yaw, pitch = rays[0]
+                look(yaw, pitch)
+                print(f"Looked at warp {wx, wy, wz}")
+            else:
+                print(f"No ray hit for warp {wx, wy, wz}")
             minescript.player_press_use(True)
             time.sleep(0.1)
             minescript.player_press_use(False)
             minescript.player_press_sneak(False)
             time.sleep(0.5)
 
-    def go(area_name, pre_points=(), final_dist=1.5):
+
+    def go(area_name, pre_points=(), go_to_area=True):
         for point in pre_points:
-            path_walk_to(point, distance=1.5)
+            path_walk_to(point)
             time.sleep(0.1)
-        for pos, name in AREA_LIST.items():
-            if name == area_name:
-                path_walk_to(pos, distance=final_dist)
-                break
+        if go_to_area:
+            for pos, name in AREA_LIST.items():
+                if name == area_name:
+                    path_walk_to(pos)
+                    return
+
 
     if area == "Lava Springs":
-        go(area, pre_points=[(4, 147, -29)])
+        go(area, pre_points=[(4, 147, -29)], go_to_area=False)
         warp(area)
         go(area)
 
     elif area == "Upper Mines":
-        go(area, pre_points=[(-2, 147, -19)])
+        go("Upper Mines", pre_points=[(-2, 147, -19)], go_to_area=False)
         warp(area)
         go(area)
 
@@ -122,7 +138,7 @@ def mining():
             if minescript.get_block(*block) not in BLOCK_LIST:
                 continue
             yaw, pitch = angles_for_block[0]
-            look_human(yaw, pitch)
+            look(yaw, pitch)
             while minescript.get_block(*block) in BLOCK_LIST:
                 minescript.player_press_attack(True)
         time.sleep(0.05)
