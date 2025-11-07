6b71520abd77cf6fc7413918e4ef2dfe11112ec9
hyshenn <135244915+hyshenn@users.noreply.github.com>
2025-11-07 14:50:34 +0200

    Update commissions.py

--- commissions.py
+++ commissions.py
@@ -6,6 +6,7 @@ from ThetaStar import path_walk_to
 from raycast import raycast_block_subregions
 import threading, time, math
 
+ClickType = JavaClass("net.minecraft.world.inventory.ClickType")
 Minecraft = JavaClass("net.minecraft.client.Minecraft")
 
 BLOCK_LIST = [
@@ -16,9 +17,6 @@ BLOCK_LIST = [
     "minecraft:prismarine",
     "minecraft:polished_diorite"
         ]
-
-def distance_3d(a, b):
-    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
 
 #Get the commissions from the tablist
 def get_commissions():
@@ -55,9 +53,8 @@ def travel_to_commission():
         "Upper Mines": [(-33, 174, -31), (-66, 222, -50), (-114, 196, -30)]
     }
 
-    #commissions = get_commissions()
-    #area = next((a for c in commissions for a in AREA_LIST.values() if a in c.strip()), None)
-    area = "Upper Mines"
+    commissions = get_commissions()
+    area = next((a for c in commissions for a in AREA_LIST.values() if a in c.strip()), None)
     if not area:
         return
     #helpers
@@ -79,7 +76,6 @@ def travel_to_commission():
             minescript.player_press_sneak(False)
             time.sleep(0.5)
 
-
     def go(area_name, pre_points=(), go_to_area=True):
         for point in pre_points:
             path_walk_to(point)
@@ -97,7 +93,7 @@ def travel_to_commission():
         go(area)
 
     elif area == "Upper Mines":
-        go("Upper Mines", pre_points=[(-2, 147, -19)], go_to_area=False)
+        go(area, pre_points=[(-2, 147, -19)], go_to_area=False)
         warp(area)
         go(area)
 
@@ -109,6 +105,49 @@ def travel_to_commission():
 
     elif area == "Rampart's Quarry":
         go(area)
+
+    elif area not in AREA_LIST and "Mine" in area:
+        go(None, pre_points=[(23, 145, -26)], go_to_area=False)
+
+#claim commissions via shift clicking them
+def claim_commissions():
+    time.sleep(0.5)
+    screen = Minecraft.getInstance().screen
+    if screen is None:
+        return
+    container_menu = screen.getMenu()
+    items = minescript.container_get_items()
+    commission_slots = [item.slot for item in items if item.item == "minecraft:writable_book"]
+    mouse_button = 0
+    for slot in commission_slots:
+        try:
+            Minecraft.getInstance().gameMode.handleInventoryMouseClick(
+                container_menu.containerId,
+                slot,
+                mouse_button,
+                ClickType.QUICK_MOVE,
+                Minecraft.getInstance().player
+            )
+            time.sleep(0.5)
+        except:
+            pass
+
+def kill_commission():
+    commissions = get_commissions()
+    target_type = next((t for c in commissions for t in ["Glacite Walker", "Goblin"] if t in c.strip()), None)
+    target_type = "Glacite Walker"
+    if not target_type:
+        return
+    available_entities = []
+    target_entities = minescript.entities(max_distance=100)
+
+    for t_entity in target_entities:
+        if target_type in t_entity.name:
+            available_entities.append(t_entity)
+
+    for entity in available_entities:
+        while entity.health > 0.1:
+            path_walk_to(entity.position)
 
 #mining loop
 def mining():
@@ -142,3 +181,4 @@ def mining():
             while minescript.get_block(*block) in BLOCK_LIST:
                 minescript.player_press_attack(True)
         time.sleep(0.05)
+kill_commission()
